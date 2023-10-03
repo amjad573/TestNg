572dd71fb241298d376c187babaa509951664af4
amjad isleem <amjadesleem@gmail.com>
2023-10-03 13:58:05 +0300

    add unittest file

--- src/test/java/AssertTest.java
+++ src/test/java/AssertTest.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class AssertTest {
-}
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AssertTest {
+
+    @BeforeClass
+    public void printBStatus(){
+        System.out.println("class is started");
+    }
+    @BeforeMethod
+    public void printBStatus(ITestResult result){
+        System.out.println(result.getMethod().getMethodName()+"is Started");
+    }
+
+    @AfterMethod
+    public void printAStatus(ITestResult result){
+        System.out.println(result.getMethod().getMethodName()+"is Finish");
+    }
+
+
+    @Test(priority=1)
+    public void testMultiplication(){
+        Assert.assertTrue(5==5);
+    }
+    @Test(priority=2)
+    public void testDivision(){
+        Assert.assertTrue(5==5);
+    }
+
+@Test
+public void testRandom(){
+    throw new SkipException("Thow new skipp");
+}}
